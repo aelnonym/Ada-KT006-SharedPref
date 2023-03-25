b83dd71ec7c0d7b64c19611bc535040c05ec1789
Laura Lanna Brandão <laura.lannab@gmail.com>
2023-03-25 18:52:48 -0300

    login screen layout

--- app/src/main/java/com/theapache64/composeandroidtemplate/ui/screen/MainViewModel.kt
+++
@@ -1,8 +0,0 @@
-package com.theapache64.composeandroidtemplate.ui.screen
-
-import androidx.lifecycle.ViewModel
-import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
-
-@HiltViewModel
-class MainViewModel @Inject constructor() : ViewModel()
